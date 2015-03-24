b1fa6c177cb1809df986abacda7042ff81fe63a9
Duy Tran <drytuna@gmail.com>
2015-03-24 10:29:20 -0400

    finished sample
    finished sample

--- src/webservices/demo/ScoreService.java
+++ src/webservices/demo/ScoreService.java
@@ -1,14 +1,39 @@
 package webservices.demo;
 
+import java.util.HashMap;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService(name = "ScoreService", serviceName = "ScoreWebService")
 public class ScoreService {
-
+	private HashMap<String, Double> scores = new HashMap<String, Double>();
+	
+	public ScoreService() {
+		scores.put("John", 90.5);
+		scores.put("Michael", 100.0);
+		scores.put("Michelle", 98.5);
+	}
+	
 	@WebMethod()
 	public String sayHello(String name) {
 	    System.out.println("Hello: " + name);
 	    return "Hello " + name + "!";
+	}
+	
+	@WebMethod(operationName = "findScore")
+	public double findScore(String name) {
+		Double d = scores.get(name);
+		if (d == null) {
+			System.out.println("Student " + name
+					+ " is not found ");
+			return -1;
+		}
+		else {
+			System.out.println("Student " + name
+					+ "\'s score is "
+					+ d.doubleValue());
+			return d.doubleValue();
+		}
 	}
 }
